1aa0e1e27e4bb5cd2f7acff6e72afb1b6afad728
Paolo Chang <pchang@architech.ca>
2022-03-04 11:47:06 -0500

    4.6 Module

---
+++ 4-node-modules/4-06-module.js
@@ -0,0 +1,8 @@
+const counter = require("./4-6-counter.js");
+
+/** MUST import 4-6-counter.js */
+// console.log(count);
+// console.log(getCount());
+
+counter.increase();
+console.log(counter.getCount());
