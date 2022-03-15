e0742651ac47aaaa9893b7d075b8ec50e771dae5
Paolo Chang <pchang@architech.ca>
2022-03-15 14:23:08 -0400

    To rename folders

--- 4-node-modules/4-06-module.js
+++
@@ -1,8 +0,0 @@
-const counter = require("./4-06-counter.js");
-
-/** MUST import 4-6-counter.js */
-// console.log(count);
-// console.log(getCount());
-
-counter.increase();
-console.log(counter.getCount());
