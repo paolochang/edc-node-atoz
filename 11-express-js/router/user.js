de00ee827f3bc8519655b3220ad9402146508a7a
Paolo Chang <pchang@architech.ca>
2022-03-17 17:13:38 -0400

    11.10 express route

---
+++ 11-express-js/router/user.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(201).send("GET: /users");
+});
+
+router.post("/", (req, res) => {
+  res.status(201).send("POST: /users");
+});
+
+router.put("/:id", (req, res) => {
+  res.status(201).send("PUT: /users");
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(201).send("DELETE: /users");
+});
+
+export default router;
