f2d48e812a146263e52c1625feffc61f42b81249
Paolo Chang <pchang@architech.ca>
2022-03-28 23:35:15 -0400

    21.4-twitter-sequelize-auth

--- 12-twitter/server/db/database.js
+++ 12-twitter/server/db/database.js
@@ -1,3 +1,4 @@
+import mysql from "mysql2";
 import { config } from "../config.js";
 import SQ from "sequelize";
 
@@ -6,3 +7,12 @@ export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
   dialect: "mysql",
 });
+
+const pool = mysql.createPool({
+  host,
+  user,
+  database,
+  password,
+});
+
+export const db = pool.promise();
