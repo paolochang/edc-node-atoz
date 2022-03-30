10f7680d134c30ea3ef575b15bf2c3069b5fbbd0
Paolo Chang <pchang@architech.ca>
2022-03-30 19:19:26 -0400

    23.4 twitter mongoose auth

--- 12-twitter/server/data/auth.js
+++ 12-twitter/server/data/auth.js
@@ -1,26 +1,26 @@
-import MongoDb from "mongodb";
-import { getUsers } from "../db/database.js";
+import Mongoose from "mongoose";
+import { useVirtualId } from "../db/database.js";
 
-const ObjectId = MongoDb.ObjectId;
+const userSchema = new Mongoose.Schema({
+  username: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  url: String,
+});
+
+useVirtualId(userSchema);
+
+const User = Mongoose.model("User", userSchema);
 
 export async function create(user) {
-  return getUsers()
-    .insertOne(user)
-    .then((data) => data.insertedId.toString());
+  return new User(user).save().then((data) => data.id);
 }
 
 export async function findByUsername(username) {
-  return getUsers()
-    .findOne({ username }) //
-    .then(mapOptionalUser);
+  return User.findOne({ username });
 }
 
 export async function findById(id) {
-  return getUsers()
-    .findOne({ _id: new ObjectId(id) })
-    .then(mapOptionalUser);
-}
-
-function mapOptionalUser(user) {
-  return user ? { ...user, id: user._id.toString() } : user;
+  return User.findById(id);
 }
